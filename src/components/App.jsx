45794d6517de1f8a97ecdf6d74cd78d10f059f34
MikkyMeow <arturakk2015@gmail.com>
2022-01-27 00:25:53 +0300

    deploy webpack config

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="wrapper">React is working</div>;
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import "./index.less";
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App.jsx";
+import "./styles/index.less";
+
+render(<App />, document.getElementById("root"));
